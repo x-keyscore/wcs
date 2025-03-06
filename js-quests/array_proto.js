57e6b4b630cb3108bc4703ba67a191db3066a0ef
x-keyscore <59397349+x-keyscore@users.noreply.github.com>
2025-03-06 16:41:24 +0100

    Update array_proto.js
    save

--- js-quests/array_proto.js
+++ js-quests/array_proto.js
@@ -6,11 +6,11 @@ const step1 = mysteriousString.split("");
 console.log('step 1 : ', step1);
 
 // step2 : get a slice of the array : take elements from the 15th included to the 32nd excluded (remember indexes start at 0 !)
-const step2 = step1.slice(15, 33);
+const step2 = step1.slice(14, 31);
 console.log('step 2 : ', step2);
 
 // step3 : Splice the array to replace 2 elements from index 5 with only one element : the letter 't'
-const step3 = step2.splice(4, 2, "t", "t");
+const step3 = step2.splice(5, 2, "t");
 
 console.log('step 3 : ', step2);
 
