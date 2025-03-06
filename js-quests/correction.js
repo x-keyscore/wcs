cfdf0fbb83455dc886dcd94734bf1665dad46225
x-keyscore <antonin.raymondaud@gmail.com>
2025-03-06 17:58:22 +0100

    save

---
+++ js-quests/correction.js
@@ -0,0 +1,11 @@
+// Exo 1
+const hiddenMessage = ["X", "X", "X", "X", "W", "X", "E",  "X", "X", "X", "X", "X", "L", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "L", "X", "X", "X", "X", "X", "X", "X", "X", "X", " ","X", "X", "X", "X", "X", "X", "X", "X", "D", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "O", "X", "X", "X", "X", "X", "X", "N", "X", "X", "X", "X", "E", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", ",", "X", " ", "X"]
+
+let message = "";
+for (let i = 0; i < hiddenMessage.length; i++) {
+    if (hiddenMessage[i] !== "X") {
+        message += hiddenMessage[i];
+    }
+}
+console.log("Exo 1 :");
+console.log(message);
