900271f832f764326a6cbbe8094be7d19dd62c86
x-keyscore <antonin.raymondaud@gmail.com>
2025-03-06 12:30:20 +0100

    save

---
+++ js-quests/var_naming.js
@@ -0,0 +1,14 @@
+
+const name = "David";
+const lastname = "Bruno";
+
+let bio = "Hi, I'm David Bruno from SF, I like to cook and meet new people.";
+
+let profile_img = "http://www.go.com/davif.png";
+
+let city = "San Francisco";
+
+let followers = 109;
+let following = 200;
+
+following++;
